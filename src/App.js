07f4ab245dc0a74960efa34ac072f38098d529ae
alvalenda <banysan@msn.com>
2022-08-31 19:44:13 -0300

    +header

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
+import Header from "./components/Header";
+
 function App() {
-  const title = "Blog Post";
-  const body = "This is a blog post";
-  const comments = [
-    { id: 1, text: "This is a comment" },
-    { id: 2, text: "This is another comment" },
-    { id: 3, text: "This is a third comment" },
-  ];
-
-  const loading = false;
-  const showComments = true;
-
-  if (loading) return <h1>Loading...</h1>;
-
-  const commentBlock = (
-    <div className="comments">
-      <h3> Comments ({comments.length})</h3>
-      <ul>
-        {comments.map((comment) => (
-          <li key={comment.id}>{comment.text}</li>
-        ))}
-      </ul>
-    </div>
-  );
   return (
     <>
-      <h1> {title.toUpperCase()} </h1>
-      <p>{body}</p>
-
-      {showComments && commentBlock}
+      <Header text="Hello World!" />
+      <div className="container">
+        <h1> Olá Mundo do React </h1>
+      </div>
     </>
   );
 }
