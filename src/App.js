e50f9c52e73fa6a1869697086b3a1b9878d342b3
alvalenda <banysan@msn.com>
2022-08-31 19:06:40 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const title = "Blog Post";
+  const body = "This is a blog post";
+  const comments = [
+    { id: 1, text: "This is a comment" },
+    { id: 2, text: "This is another comment" },
+    { id: 3, text: "This is a third comment" },
+  ];
+
+  const loading = false;
+  const showComments = true;
+
+  if (loading) return <h1>Loading...</h1>;
+
+  const commentBlock = (
+    <div className="comments">
+      <h3> Comments ({comments.length})</h3>
+      <ul>
+        {comments.map((comment) => (
+          <li key={comment.id}>{comment.text}</li>
+        ))}
+      </ul>
     </div>
+  );
+  return (
+    <>
+      <h1> {title.toUpperCase()} </h1>
+      <p>{body}</p>
+
+      {showComments && commentBlock}
+    </>
   );
 }
 
