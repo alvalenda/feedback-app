0bf13ae78c60fd076e10f5c66955a615928195e2
alvalenda <banysan@msn.com>
2022-09-01 19:34:10 -0300

    comment

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -3,7 +3,7 @@ import FeedbackItem from "./FeedbackItem";
 function FeedbackList({ feedback }) {
   if (!feedback || feedback.length === 0) return <p>No Feedback Yet</p>;
 
-  console.log(feedback);
+  //   console.log(feedback);
   return (
     <div className="feedback-list">
       {feedback.map((item) => (
