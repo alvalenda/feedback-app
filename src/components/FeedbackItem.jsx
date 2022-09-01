dfa4af44623724fc2e8f692e424dfa921111fb5a
alvalenda <banysan@msn.com>
2022-09-01 18:56:09 -0300

    testing: setRating and prev values

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -4,10 +4,17 @@ function FeedbackItem() {
   const [rating, setRating] = useState(7);
   const [text, setText] = useState("This is an example of a Feedback Item");
 
+  const handleClick = (e) => {
+    setRating((prev) => {
+      return prev + 1;
+    });
+  };
+
   return (
     <div className="card">
       <div className="num-display">{rating}</div>
       <div className="text-display">{text}</div>
+      <button onClick={handleClick}> Click </button>
     </div>
   );
 }
