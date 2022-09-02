cc9e90baf341b8d50ab93aca4b91ef0612675fb0
alvalenda <banysan@msn.com>
2022-09-02 12:12:55 -0300

    feat: deleteFeedback

--- src/App.js
+++ src/App.js
@@ -7,12 +7,17 @@ import Card from "./components/shared/Card";
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
 
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete Feedback " + id + "?"))
+      setFeedback(feedback.filter((item) => item.id !== id));
+  };
+
   return (
     <>
       <Header />
       <div className="container">
         <h1> Olá Mundo do React </h1>
-        <FeedbackList feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
         <Card> Hello World of Card </Card>
       </div>
     </>
