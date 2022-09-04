71f51bc3ad42032cec3671743b288b512d60f0a5
alvalenda <banysan@msn.com>
2022-09-04 20:44:19 -0300

    feat: addFeedback

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,53 +1,49 @@
-import { useState } from "react";
-import Card from "./shared/Card";
-import Button from "./shared/Button";
-import RattingSelect from "./RattingSelect";
+import { useState } from 'react'
+import Card from './shared/Card'
+import Button from './shared/Button'
+import RattingSelect from './RattingSelect'
 
-const FeedbackForm = () => {
-  const [text, setText] = useState("");
-  const [rating, setRating] = useState(10);
-  const [message, setMessage] = useState("");
-  const [btnDisabled, setBtnDisabled] = useState(true);
+const FeedbackForm = ({ handleAdd }) => {
+  const [text, setText] = useState('')
+  const [rating, setRating] = useState(10)
+  const [message, setMessage] = useState('')
+  const [btnDisabled, setBtnDisabled] = useState(true)
 
   const handleTextChange = (e) => {
-    const newText = e.target.value;
-    setText(newText);
+    const newText = e.target.value
+    setText(newText)
 
-    if (newText === "") {
-      setBtnDisabled(true);
-      setMessage(null);
-      return;
+    if (newText === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+      return
     }
-    if (newText !== "" && newText.trim().length < 10) {
-      setBtnDisabled(true);
-      setMessage("Text must be at least 10 characters long");
-      return;
+    if (newText !== '' && newText.trim().length < 10) {
+      setBtnDisabled(true)
+      setMessage('Text must be at least 10 characters long')
+      return
     }
-    setBtnDisabled(false);
-    setMessage(null);
-  };
-
-  const addFeedback = () => {};
+    setBtnDisabled(false)
+    setMessage(null)
+  }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    if (text.trim().length >= 10) {
-      const newFeedback = {
-        text,
-        rating,
-      };
-      //   if (feedbackEdit.edit === true) {
-      //     updateFeedback(feedbackEdit.item.id, newFeedback);
-      //   } else {
-      //     addFeedback(newFeedback);
-      //   }
-      addFeedback(newFeedback);
-      // NOTE: reset to default state after submission
-      //   setRating(10);
-      setBtnDisabled(true);
-      setText("");
-    }
-  };
+    e.preventDefault() // since it's a submit, need prevent the default behavior wich is submitting to the actual file.
+    if (text.trim().length < 10) return
+
+    const newFeedback = { text, rating }
+
+    //   if (feedbackEdit.edit === true) {
+    //     updateFeedback(feedbackEdit.item.id, newFeedback);
+    //   } else {
+    //     addFeedback(newFeedback);
+    //   }
+
+    handleAdd(newFeedback)
+    //   setRating(10);
+    setBtnDisabled(true)
+    setText('')
+  }
 
   return (
     <Card>
@@ -57,21 +53,21 @@ const FeedbackForm = () => {
           select={(rating) => setRating(rating)}
           selected={rating}
         />
-        <div className="input-group">
+        <div className='input-group'>
           <input
             onChange={handleTextChange}
-            type="text"
-            placeholder="Write a review"
+            type='text'
+            placeholder='Write a review'
             value={text}
           />
-          <Button type="submit" isDisabled={btnDisabled}>
+          <Button type='submit' isDisabled={btnDisabled}>
             Send
           </Button>
         </div>
-        {message && <div className="message">{message}</div>}
+        {message && <div className='message'>{message}</div>}
       </form>
     </Card>
-  );
-};
+  )
+}
 
-export default FeedbackForm;
+export default FeedbackForm
