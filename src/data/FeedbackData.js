1cc05a3f987209cedf9f9e93656a0736458a0881
alvalenda <banysan@msn.com>
2022-09-04 20:43:45 -0300

    feat: findFreeId

--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -1,19 +1,40 @@
-const FeedbackData = [
+export const feedbackData = [
   {
     id: 1,
     rating: 10,
-    text: "lorem ipsum dolor sit amet, consectetur adip. consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    text: 'lorem ipsum dolor sit amet, consectetur adip. consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit',
   },
   {
     id: 2,
     rating: 9,
-    text: "lorem ipsum dolor sit amet, consectetur adip. consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    text: 'lorem ipsum dolor sit amet, consectetur adip. consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit',
   },
   {
     id: 3,
     rating: 8,
-    text: "lorem ipsum dolor sit amet, consectetur adip. consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    text: 'lorem ipsum dolor sit amet, consectetur adip. consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit',
   },
-];
+]
 
-export default FeedbackData;
+export const findFreeId = (list) => {
+  const sortedById = listSortedById(list)
+  let previousId = 0
+
+  for (const item of sortedById) {
+    if (item.id !== previousId + 1) {
+      return previousId + 1
+    }
+    previousId = item.id
+  }
+
+  // console.log(previousId + 1)
+  return previousId + 1
+}
+
+const listSortedById = (list) => {
+  return list.slice().sort((a, b) => a.id - b.id)
+}
+
+export const FeedbackData = { feedbackData, findFreeId }
+
+export default FeedbackData
